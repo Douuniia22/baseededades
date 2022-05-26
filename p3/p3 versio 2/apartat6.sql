af2b51fff4e67fefc550f67671c9a75170224e3a
Douuniia22 <102051122+Douuniia22@users.noreply.github.com>
2022-05-26 17:44:16 +0200

    Update apartat6.sql

--- p3/p3 versio 2/apartat6.sql
+++ p3/p3 versio 2/apartat6.sql
@@ -1,14 +1,12 @@
 -- script que fa la consulta 6
--- Creeu una vista que obtingui el llistat d’empleats amb el codi de la zona que té
--- assignada. Tingueu en compte que també volem que els responsables de zona
--- apareguin com a empleats assignats, i que no volem que cap empleat aparegui més
--- d’una vegada.
+-- Creeu una vista que obtingui el llistat dâ€™empleats amb el codi de la zona que tÃ©
+-- assignada. Tingueu en compte que tambÃ© volem que els responsables de zona
+-- apareguin com a empleats assignats, i que no volem que cap empleat aparegui mÃ©s
+-- dâ€™una vegada.
 
-create view llista_empleats AS
-SELECT DISTINCT empl_ord, zona, responsable
-FROM assignacions, zones_biocontencio;
+select num_pass, zona_assignada from qualificats
+UNION
+select empl_ord, zona from assignacions;
 
-
-SELECT * FROM llista_empleats;
 
 source apartat7.sql;
