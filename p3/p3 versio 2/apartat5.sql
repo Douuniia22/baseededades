fc0ddb68376ea5eabdb93f6ffa18af8270f4ba14
Douuniia22 <102051122+Douuniia22@users.noreply.github.com>
2022-05-26 17:43:13 +0200

    Update apartat5.sql

--- p3/p3 versio 2/apartat5.sql
+++ p3/p3 versio 2/apartat5.sql
@@ -1,2 +1,13 @@
 
+
+select  nom, num_pass
+from assignacions, empleats
+where num_pass=empl_ord
+group by empl_ord
+having COUNT(empl_ord)> (select COUNT(empl_ord)
+						from assignacions, empleats 
+						where nom= "Jordi Guasch" and num_pass=empl_ord
+						group by empl_ord
+						having (count(empl_ord)));
+            
 source apartat5.sql;
