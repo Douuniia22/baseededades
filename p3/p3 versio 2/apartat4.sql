e3238d90aaf9daf0cdcfb59b587e199ad5d3269c
Douuniia22 <102051122+Douuniia22@users.noreply.github.com>
2022-05-26 17:42:43 +0200

    Update apartat4.sql

--- p3/p3 versio 2/apartat4.sql
+++ p3/p3 versio 2/apartat4.sql
@@ -1,4 +1,10 @@
 -- script per a la consulta 4
 
+select num_pass from ordinaris
+where num_pass in(select empl_ord 
+		  from assignacions
+		  where lab in( select codi
+						from laboratoris
+						where nom = 'BCN-ARBRE'));
 
 source apartat5.sql;
